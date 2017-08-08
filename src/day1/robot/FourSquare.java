7f77ff6c6c77d0e8248fbeb758af5241a195b4c2
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-08 11:51:15 -0700

    day 2
    fun fun fun  fun fun fun fun  funvfun fun fun  funfun fun fun  funfun
    fun fun  funfun fun fun  funvfun fun fun  funfun fun fun  funvvfun fun
    fun  funfun fun fun  funvfun fun fun  funfun fun fun  funvfun fun fun
    funvfun fun fun  funvfun fun fun  funfun fun fun  funvvfun fun fun  funv

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -1,14 +1,16 @@
 package day1.robot;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot dabest = new Robot("batman");
 
-	void go() {
+	void go() {drawSquare();
 		// 4. Make the robot move as fast as possible
 
 		// 5. Set the pen width to 5
@@ -18,7 +20,7 @@ public class FourSquare {
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
@@ -26,11 +28,18 @@ public class FourSquare {
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
-		
+    for(int i=0;i<1000;i++){
+    	dabest.show();
+    	dabest.hide();
+    	dabest.sparkle();
+    	dabest.setRandomPenColor();
+     dabest.setSpeed(10);
+     dabest.penDown();
+     dabest.move(6);
+     dabest.turn(4);
+     dabest.turn(2);
 	}
-
+	}
 	public static void main(String[] args) {
 		new FourSquare().go();
 	}
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,8 +7,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
+		Robot dude = new Robot("mini");
+		dude.setSpeed(10);
+		dude.setRandomPenColor();
+		dude.penDown();
+		dude.move(200);
+		dude.turn(100);
+		dude.move(200);
+	    dude.turn(100);
 		
 		
 	}
