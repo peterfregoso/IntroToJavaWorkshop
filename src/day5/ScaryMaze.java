9c333b1c858523249985d23aa908ffd80a7c1265
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-11 11:29:08 -0700

    scary
    fun fun fun fun fun fun fun fun fun fun

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -2,7 +2,6 @@ package day5;
 
 import java.applet.AudioClip;
 import java.awt.Dimension;
-
 import java.awt.Graphics;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
@@ -16,20 +15,22 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
 	final int frameWidth = 600;
-	final int frameHeight = 400;
+	final int frameHeight = 492;
+	int backgroundcolor = -1;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
-		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 1. Use this online tool to make a maze image and drop it into your
+		// day5 package: http://pixlr.com/editor/
+		maze = ImageIO.read(getClass().getResource("maze.gif"));
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		new Robot().mouseMove(200, 300);
+		// 3. add a mouse motion listener using:
+		// addMouseMotionListener(this)
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -37,36 +38,41 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
+		// 4. print the mouseColor variable to see what color the mouse is
+		// touching
+		System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		// 6. if the mouse falls off the path (if it is on the background)
+		if (mouseColor != backgroundcolor) {
+			scare();
+		}
+		// call the scare method
 
-		//5. make a variable to hold the background color. 
+		// 10. if the mouse is on the end color
 
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
+		// pop up a message to tell them they won
+
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		// 7. find a scary sound and put it in the day5 package where you put
+		// your maze picture. You can find a sound on freesound.org. Log in as
+		// leagueofamazing/code4life.
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
 
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+		sound.play();
+		// 9. drop an image into your day5 package, and use the showScaryImage
+		// method to scare your victim!
+		showScaryImage("eli.jpg");
 	}
 
-	private void showScaryImage(String imageName) {
+	private void showScaryImage(String eli) {
 		try {
-			maze = ImageIO.read(getClass().getResource(imageName));
+			maze = ImageIO.read(getClass().getResource(eli));
 		} catch (Exception e) {
-			System.err.println("Couldn't find this image: " + imageName);
+			System.err.println("Couldn't find his image: " + eli);
 		}
 		repaint();
 	}
@@ -92,9 +98,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
